4ea84df58396df4d5bced30f46b88e599ea4d97e
Subhash Gupta <subhashgupta@subhash-guptas-macbook-pro.local>
2010-05-08 18:11:22 +0100

    Sub#

--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
-require 'appengine-rack'
+log = File.new('log/sinatra.log', 'a')
+$stdout.reopen(log)
+$stderr.reopen(log)
+
 require 'ishareweb'
-
-AppEngine::Rack.configure_app(:application => "i-share-web", :version => 3)
-
 run Sinatra::Application
---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application,         "ishareweb"
+
+set :deploy_to,           "/var/www/#{application}"
+set :domain,              "li152-25.members.linode.com"
+set :repository_cache,    "#{application}_cache"
+set :environment,         "production"
+  
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+set :local_shared_dirs,   %w(public/uploads)
+
+set :repository,          "git@github.com:letmebuy/I-Share-Web.git"
+set :keep_releases,       5
+set :user,                "jeero"
+set :scm_user,            "deployer"
+set :deploy_via,          :remote_cache
+set :scm,                 :git
+set :runner,              "deploy"
+ssh_options[:forward_agent] = true
+set :use_sudo,               true
+
+# comment out if it gives you trouble. newest net/ssh needs this set.
+ssh_options[:paranoid] = false
+
+after "deploy", "deploy:cleanup"
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "sudo /etc/init.d/nginx restart"
+  end
+end
