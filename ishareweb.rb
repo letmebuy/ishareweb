8a43df065f619f5b8a989f12d2ed2fcd4f19464d
Subhash Gupta <subhashgupta@subhash-guptas-macbook-pro.local>
2010-10-04 09:30:57 +0100

    SuB#

--- ishareweb.rb
+++ ishareweb.rb
@@ -50,7 +50,7 @@ get '/latest' do
   @page = (is_blank?(params[:page]) ? 1 : params[:page].to_i)
   @country = (is_blank?(params[:country]) ? nil : params[:country])
   @total, @results = Google.latest(@query, @page, @ip_address, @country)
-  erb :new_search
+  erb :search
 end
 
 get '/privacy' do
